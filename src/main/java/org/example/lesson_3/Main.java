5cea50c45326a18722745992cbac44a277f9f616
Stanislau Schastny <stasschastny@gmail.com>
2024-12-06 18:18:44 +0100

    update "println"

--- src/main/java/org/example/lesson_3/Main.java
+++ src/main/java/org/example/lesson_3/Main.java
@@ -3,6 +3,6 @@ package org.example.lesson_3;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Git!!");
-        System.out.println("Hello Reset! ");
+        System.out.println("Hello Reset!");
     }
 }
